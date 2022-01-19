50f6369f1b8315a74d3ece6ca040e8e0c81c7a9c
ivanfbj <ivanfbj@gmail.com>
2022-01-18 20:04:19 -0500

    Clase para colores en consola

---
+++ main.py
@@ -0,0 +1,11 @@
+from bcolors import bcolors
+
+print(f'Color por DEFECTO.')
+print(f'{bcolors.OK}Color VERDE.{bcolors.RESET}')
+print(f'{bcolors.WARNING}Color AMARILLO.{bcolors.RESET}')
+print(f'{bcolors.FAIL}Color ROJO.{bcolors.RESET}')
+print(f'{bcolors.HIGHLIGHTER}Letra en color BLANCO fondo en Color VERDE.{bcolors.RESET}')
+print(f'{bcolors.BOLD_RED}Negrita en Color ROJO.{bcolors.RESET}')
+
+
+
